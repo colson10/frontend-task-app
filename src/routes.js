5bc0d06e6fd66cdd64b7dee2b5e7d37c01365f3e
Carl Olson <colson10@gmail.com>
2018-08-22 13:56:27 -0700

    reverted to custom domain

--- src/routes.js
+++ src/routes.js
@@ -4,8 +4,8 @@ const ROUTES = {
 };
 
 // const API_URL = 'http://localhost:4000';
-// const API_URL = 'http://api.listsublist.pro';
-const API_URL = 'https://list-sublist-backend.herokuapp.com';
+const API_URL = 'http://api.listsublist.pro';
+// const API_URL = 'https://list-sublist-backend.herokuapp.com';
 
 
 export { ROUTES, API_URL };
