7ba8ae9a7adfdc46b47ed261550af6462b7a76ff
Carl Olson <colson10@gmail.com>
2018-08-22 11:46:42 -0700

    reverting back to router on backend

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 'use strict';
 
-const cors = require('cors');
+// const cors = require('cors');
 const express = require('express');
-const googleRouter = require('./google-router');
+// const googleRouter = require('./google-router');
 require('dotenv').config();
 
 const app = express();
 
-app.use(cors({
-  credentials: true,
-  origin: ['http://localhost'],
-}));
+// app.use(cors({
+//   credentials: true,
+//   origin: ['http://localhost'],
+// }));
 app.use(express.static(`${__dirname}/build`));
-app.use(googleRouter);
+// app.use(googleRouter);
 
 app.listen(process.env.PORT, () => {
   console.log(__dirname, 'this is the dirname');
--- src/components/landing/landing.js
+++ src/components/landing/landing.js
@@ -2,12 +2,12 @@ import React from 'react';
 import { Redirect } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import { ROUTES, FRONT_END_API_URL } from '../../routes';
+import { ROUTES, API_URL } from '../../routes';
 
 let redirect = false;
 let destinationRoute = null;
 
-const googleLink = `https://accounts.google.com/o/oauth2/v2/auth?redirect_uri=${FRONT_END_API_URL}/oauth/google&scope=openid%20email%20profile&client_id=391898223520-nodlikiik4v7118fhe3lofn9n3irl3kd.apps.googleusercontent.com&prompt=consent&response_type=code`;
+const googleLink = `https://accounts.google.com/o/oauth2/v2/auth?redirect_uri=${API_URL}/oauth/google&scope=openid%20email%20profile&client_id=391898223520-nodlikiik4v7118fhe3lofn9n3irl3kd.apps.googleusercontent.com&prompt=consent&response_type=code`;
 
 
 class Landing extends React.Component {
